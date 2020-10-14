f3428952ffd7c23611415afe7632483da1b896b3
kamillarazikhova <53394768+kamillarazikhova@users.noreply.github.com>
2020-10-14 15:11:52 +0500

    Update HotelchainApplication.java

--- CSCI-361-term-project/src/test/java/com/example/hotelchain/HotelchainApplication.java
+++ CSCI-361-term-project/src/test/java/com/example/hotelchain/HotelchainApplication.java
@@ -1,4 +1,4 @@
-package com.hedgehogs.hotelchain;
+package com.example.hotelchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
