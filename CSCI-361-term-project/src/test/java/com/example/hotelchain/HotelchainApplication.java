2c9c8ba4b292d1533d02344bac68149da47da54d
Altynay18 <40347560+Altynay18@users.noreply.github.com>
2020-10-14 16:31:54 +0600

    Revert "Delete HotelchainApplication.java"
    This reverts commit 496c99217880ee46688a3b274a1dc6afc6a89e4f.

--- CSCI-361-term-project/src/test/java/com/example/hotelchain/HotelchainApplication.java
+++ CSCI-361-term-project/src/test/java/com/example/hotelchain/HotelchainApplication.java
@@ -1,4 +1,4 @@
-package com.example.hotelchain;
+package com.hedgehogs.hotelchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
