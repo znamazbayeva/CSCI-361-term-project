dc1bd4a2e98ba9ed9f1f5dd0f7e6029761409437
kamillarazikhova <53394768+kamillarazikhova@users.noreply.github.com>
2020-10-14 15:09:08 +0500

    Add files via upload

---
+++ CSCI-361-term-project/src/test/java/com/example/hotelchain/HotelchainApplication.java
@@ -0,0 +1,13 @@
+package com.hedgehogs.hotelchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelchainApplication.class, args);
+	}
+
+}
