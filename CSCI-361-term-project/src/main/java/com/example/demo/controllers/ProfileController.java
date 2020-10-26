f8d990d85229e885182f5797bc861243292e4434
Altynay18 <40347560+Altynay18@users.noreply.github.com>
2020-10-26 19:52:45 +0600

    Login, Registration and Profile controller added

---
+++ CSCI-361-term-project/src/main/java/com/example/demo/controllers/ProfileController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/profile")
+public class ProfileController {
+}
