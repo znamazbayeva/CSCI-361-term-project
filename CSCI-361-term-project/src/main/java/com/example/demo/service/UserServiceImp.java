04d3850932f17158f34cae7fc33d73530b137a09
Altynay18 <40347560+Altynay18@users.noreply.github.com>
2020-11-09 01:29:18 +0600

    Add files via upload

---
+++ CSCI-361-term-project/src/main/java/com/example/demo/service/UserServiceImp.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.data.Role;
+import com.example.demo.data.User;
+import com.example.demo.data.RoleRepository;
+import com.example.demo.data.UserRepository;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    BCryptPasswordEncoder encoder;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setStatus("VERIFIED");
+        Role userRole = roleRepository.findByRoleName("SITE_USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+
+    @Override
+    public boolean isUserAlreadyPresent(User user) {
+        // Try to implement this method, as assignment.
+        return false;
+    }
+
+}
