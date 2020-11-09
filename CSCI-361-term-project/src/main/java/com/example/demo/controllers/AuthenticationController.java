29c43b8efb048dd90a8ea2c9a0a91867b0857ef0
iamhappyXD <aminashaikym@gmail.com>
2020-11-09 16:56:14 +0600

    amina

---
+++ CSCI-361-term-project/src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -0,0 +1,66 @@
+package com.example.demo.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.data.User;
+import com.example.demo.service.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login"); // resources/template/login.html
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration"); // resources/template/register.html
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public ModelAndView registerUser(@Valid User user, BindingResult bindingResult, ModelMap modelMap) {
+        ModelAndView modelAndView = new ModelAndView();
+        // Check for the validations
+        if(bindingResult.hasErrors()) {
+            modelAndView.addObject("successMessage", "Please correct the errors in form!");
+            modelMap.addAttribute("bindingResult", bindingResult);
+        }
+        else if(userService.isUserAlreadyPresent(user)){
+            modelAndView.addObject("successMessage", "user already exists!");
+        }
+        // we will save the user if, no binding errors
+        else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User is registered successfully!");
+        }
+        modelAndView.addObject("user", new User());
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+   /* @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home"); // resources/template/home.html
+        return modelAndView;
+    }*/
+}
